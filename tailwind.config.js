06177aa367fde3f29956efea5ebca01979cc306a
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-11 12:00:43 -0500

    Add XS breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.js",
     "./components/**/*.js"
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '375px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
