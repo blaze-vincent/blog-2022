1742cf80d06ee3606ed9ba26294465a82f8c6a8a
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-11 11:58:13 -0500

    Add pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 export default function Home() {
-  return (
-    <p>page content populating main tag in layout</p>
-  )
+  return <div className="flex flex-col gap-8 max-w-screen-md text-lg">
+    <p> Commodo enim incididunt exercitation incididunt. Velit nostrud et officia esse deserunt ad culpa esse. Adipisicing voluptate fugiat esse cupidatat nisi ullamco sunt officia incididunt consectetur tempor. Duis ipsum ullamco pariatur irure sint sit id veniam minim occaecat irure ut. Culpa laborum laboris officia Lorem quis labore.  Do ut enim </p>
+    <p> elit aute. Minim labore sit adipisicing ullamco reprehenderit do sunt esse reprehenderit laboris do laboris aliqua. Magna non ut consequat consectetur esse reprehenderit. Esse labore laborum ad qui adipisicing aute eiusmod ex laborum ut. Nulla commodo esse do eu consectetur anim. Nostrud nulla labore labore cupidatat cupidatat commodo occaecat. Occaecat </p>
+    <p> fugiat mollit consectetur Lorem labore aliquip fugiat non commodo excepteur Lorem.  Amet magna ex cillum ex aliquip officia irure. Occaecat in tempor ad sunt laborum pariatur amet dolor amet voluptate. Pariatur commodo culpa eu laboris et sint. Consectetur consequat duis incididunt esse dolor incididunt ex id. Esse eiusmod eu </p>
+    <p> officia aute enim nostrud eiusmod anim minim tempor est elit sunt. Laboris pariatur ad tempor id non deserunt sit exercitation labore qui.  Amet exercitation magna quis adipisicing sunt est laboris voluptate. Sint pariatur exercitation consectetur aliqua fugiat cupidatat in velit. Deserunt quis magna commodo irure pariatur cillum ad minim </p>
+    <p> quis nulla. Duis ullamco veniam sunt mollit Lorem. Sunt dolore proident voluptate sit tempor culpa elit.  Elit culpa ea tempor tempor velit ullamco proident mollit. Nulla deserunt nisi velit quis culpa cillum sit Lorem aliqua tempor excepteur nulla aliqua culpa. Enim non duis tempor ea ex aliquip consequat. Aliquip </p>
+    <p> proident ad enim cillum ad irure irure culpa officia do ad id esse consectetur. Laboris nulla do sint esse irure incididunt labore velit enim excepteur sint. Elit laborum non anim laboris officia proident reprehenderit voluptate.  Dolore cillum culpa commodo nulla veniam incididunt nulla ea exercitation proident dolore do aliqua. </p>
+    <p> Eiusmod ipsum laborum est et. Excepteur fugiat nisi aliquip voluptate ad ut elit. Duis voluptate quis dolor tempor sit quis id. </p>
+  </div>
 }
