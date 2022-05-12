34ba1c3601c9bd18937eb246c84ca73870e5e5f9
Blaze Vincent <blaze.vincent@hotmail.com>
2022-05-12 14:13:02 -0500

    Standardize left position of gradients

--- components/gradientBackground.js
+++ components/gradientBackground.js
@@ -1,11 +1,12 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 
 export default function GradientBackground({className}){
   //parent should be positioned relatively and have overflow hidden for intended behavior
   
   //todo: try moving the left property of the element to 0(left of screen) since its currently relative to parent position
+  const thisElement = useRef(null);
   useEffect(_ => {
-    console.log()
+    thisElement.current.style.left = "" + -thisElement.current.getBoundingClientRect().x + "px"
   }, [])
-  return <div className={"absolute w-screen h-full top-0 left-0 bg-gradient-to-r from-amber-200 to-orange-400 " + className} />
+  return <div ref={thisElement} className={"absolute w-screen h-full top-0 left-0 bg-gradient-to-r from-amber-200 to-orange-400 " + className} />
 }
