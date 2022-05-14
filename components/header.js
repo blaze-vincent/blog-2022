d7d699bb78c707d355aa4e4855967d633f352059
Jam's computer <blaze.vincent@hotmail.com>
2022-05-13 20:56:05 -0500

    Fixed rounded corner of title gradient

--- components/header.js
+++ components/header.js
@@ -4,9 +4,9 @@ import GradientBackground from "./gradientBackground";
 export default function Header(){
 
   return (<header className="pl-4 flex flex-col items-start">
-    <div className="relative overflow-hidden text-neutral-900 xs:text-3xl sm:text-4xl text-2xl p-4 w-full">
+    <div className="relative overflow-hidden text-neutral-900 xs:text-3xl sm:text-4xl text-2xl p-4 w-full rounded-bl-xl">
       {/* rounded-bl works but left edge is moved to left edge of screen now */}
-      <GradientBackground className='rounded-bl'/>
+      <GradientBackground/>
       <h1 className="relative font-syne">nine-orange-ten</h1>
     </div>
     <Nav />
