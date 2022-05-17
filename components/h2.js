e195687ab850f965f5e4f95de24c89d7fe271f43
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-17 16:44:53 -0500

    Increase h2 size

--- components/h2.js
+++ components/h2.js
@@ -1,7 +1,7 @@
 import GradientBackground from './gradientBackground';
 
 export default function H2({children}){
-  return <h2 className="text-xl w-max overflow-hidden">
+  return <h2 className="xs:text-xl sm:text-2xl text-lg w-full max-w-max overflow-hidden">
     {children}
     <div className='h-px w-full relative'>
       <GradientBackground />
