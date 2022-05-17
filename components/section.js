495dfa3d8d329bbb60e148c34de91b7fa923ae63
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-17 16:45:26 -0500

    Add hover effect to sections

--- components/section.js
+++ components/section.js
@@ -1,5 +1,5 @@
 export default function Section({children}){
-  return <section className="flex flex-col gap-2">
+  return <section className="flex flex-col gap-2 relative hover:translate-x-2 transition-transform">
     {children}
   </section>
 }
