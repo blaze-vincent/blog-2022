6c17381eba100f694ee5c3705101786ec20d9fbb
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-23 16:34:47 -0500

    Change slide-changing action

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -3,9 +3,6 @@ import {useState} from 'react';
 
 export default function Carousel({className, slideObjects}){
   const [displayedSlide, setDisplayedSlide] = useState(0);
-  function controlDisplayedSlide(){
-    setDisplayedSlide(displayedSlide < slideObjects.length ? displayedSlide++ : 0)
-  }
   
   return <div className="relative w-full h-80">
     <div 
@@ -15,10 +12,28 @@ export default function Carousel({className, slideObjects}){
         left: `${-100 * displayedSlide}%`,
         transition: '0.125s'
       }}
-      onClick={controlDisplayedSlide}
     >
-        {slideObjects.map((slideObj, index) => {
-          return <Slide key={index} data={slideObj} selected={displayedSlide} order={index} />
+      {slideObjects.map((slideObj, index) => {
+        return <Slide 
+          key={index} 
+          data={slideObj} 
+          selectedSlide={displayedSlide} 
+          order={index}
+          onClick={_ => {setDisplayedSlide(index)}} 
+        />
+        })}
+      </div>
+      <div className="absolute w-full gap-6 items-center flex flex-row bottom-2 justify-center">
+        {slideObjects.map((_, key) => {
+          return <button 
+            key={key} 
+            className="w-6 h-6 rounded-full" 
+            onClick={_ => {setDisplayedSlide(key)}}
+            style={{
+              backgroundColor: key === displayedSlide ? '#171717' : 'white',
+              transition: '125ms'
+            }}  
+          />
         })}
       </div>
     </div>
--- components/carousel/slide.js
+++ components/carousel/slide.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 
-export default function Slide({selected, order, data}){
+export default function Slide({selectedSlide, order, data, onClick}){
 
   return <div
     className="w-full relative top-0 h-full"
+    style={{
+      cursor: order - selectedSlide === 0 ? 'default' : 'pointer'
+    }}
+    onClick={onClick}
   >
     <div 
       className=" w-full h-full"
       style={{
-        opacity: 1/(Math.abs(order-selected)+1),
+        opacity: 1/(Math.abs(order-selectedSlide)+1),
         transition: '0.125s'
       }}  
     >
